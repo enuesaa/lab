02442615739f7c09874eae132bc8f41908801454
nua <enuesaa@gmail.com>
2024-11-23 16:28:13 +0900

    fix(build): copy files on build using rollup event `generateBundle`

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import path from 'node:path'
+import { listProjects } from './src/lib/prototype/server/project'
+import { copyUnitImage } from './src/lib/prototype/server/image'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		{
+			name: 'file-copy',
+			async generateBundle() {
+				const projects = await listProjects()
+				for (const project of projects) {
+					await copyUnitImage(project)
+				}
+				console.log('file copied!')
+			},
+		},
+	],
 	resolve: {
 		alias: {
 			$lib: path.join(__dirname, './src/lib'),
