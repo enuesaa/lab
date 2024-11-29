788d948493057192d9b4871a201e0ddf60793e22
nua <enuesaa@gmail.com>
2024-11-30 03:29:16 +0900

    fix(UnitNav): タイトルが中央寄せになっていた

--- src/routes/prototype/[name]/UnitNav.svelte
+++ src/routes/prototype/[name]/UnitNav.svelte
@@ -11,18 +11,18 @@
 </script>
 
 <button on:click|preventDefault={handleClick}>
-	<span class="text-4xl">
+	<span class="text-3xl leading-[0.8] w-7">
 		{#if showing}
-			<SquareChevronDown class="inline" />
+			<SquareChevronDown class="inline align-middle" />
 		{:else}
-			<SquareChevronRight class="inline" />
+			<SquareChevronRight class="inline align-middle" />
 		{/if}
 	</span>
-	{unit.title}
+	<span class="break-all inline-block">{unit.title}</span>
 </button>
 
 <style lang="postcss">
 	button {
-		@apply font-semibold text-2xl mt-6 mb-2 pl-2 font-zenmaru;
+		@apply font-semibold text-2xl mt-7 mb-3 pl-2 font-zenmaru flex gap-[3px] text-left;
 	}
 </style>
