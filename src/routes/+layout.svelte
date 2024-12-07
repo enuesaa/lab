f94595833c572901af7c14438bd0b3d3cce1a00b
nua <enuesaa@gmail.com>
2024-12-07 21:39:52 +0900

    chore: update favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,6 +8,12 @@
 	<title>雑コード置き場</title>
 	<meta name="Hatena::Bookmark" content="nocomment" />
 	<link rel="icon" href="/favicon.ico" />
+	<link rel="icon" href="/favicon.svg" sizes="32x32" type="image/svg+xml" />
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+	<link rel="manifest" href="/manifest.json" />
+	<meta name="theme-color" content="#cccccc" />
 </svelte:head>
 
 <Header />
