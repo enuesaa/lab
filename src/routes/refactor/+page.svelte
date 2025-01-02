c8cc1e22816cec34232dc8eb06a8d7676126de68
nua <enuesaa@gmail.com>
2025-01-02 16:00:59 +0900

    feat(refactor): layout を調整

--- src/routes/refactor/+page.svelte
+++ src/routes/refactor/+page.svelte
@@ -7,6 +7,8 @@
 	import { createTreeViewCtl, createViewing } from '$lib/prototype/tree'
 	import UnitSep from '../prototype/[name]/UnitSep.svelte'
 	import UnitNav from './UnitNav.svelte'
+	import UnitDescription from '../prototype/[name]/UnitDescription.svelte'
+	import UnitTerminal from '../prototype/[name]/UnitTerminal.svelte'
 
 	createTreeViewCtl()
 	createViewing()
@@ -19,6 +21,20 @@
 		// これは highlight チックがベスト
 		// GitHub みたいに1行単位かな
 	// 目次も欲しいかも
+
+	const description = `curl ライクな CLI を作ってみる。
+
+とは言っても GET しかできない。
+curl とは似ても似つかないが、まあサンプルなので良いと思う。`
+
+	const terminal = `$ go run . -url https://example.com/
+<!doctype html>
+<html>
+<head>
+    <title>Example Domain</title>
+    
+// 省略
+// example.com へ HTTP GET リクエストをして、レスポンスボディを標準出力する`
 </script>
 
 <svelte:head>
@@ -33,7 +49,15 @@
 <UnitNav title='コード全体' />
 <UnitNav title='期待動作' />
 
-<section class="w-[95vw] ml-[-5vw] rounded-lg overflow-hidden">
-	<UnitSep text="</>" treeData={data.files} enableDownloader />
-	<CodeViewer treeData={data.files} firstOpen={'main.go'} />
+<section class="w-[96vw] ml-[-5vw] flex gap-1 rounded-lg overflow-hidden">
+	<div class="w-7/12">
+		<UnitSep text="</>" treeData={data.files} enableDownloader />
+		<CodeViewer treeData={data.files} firstOpen={'main.go'} />
+	</div>
+	<div class="w-5/12 border bg-white/60">
+		<UnitNav title='期待動作' />
+		<UnitDescription description={description} />
+		<UnitSep text="ターミナル" />
+		<UnitTerminal content={terminal} />
+	</div>
 </section>
