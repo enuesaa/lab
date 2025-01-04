fee9bcc427cdda02e4d5457a9f2edff01546f69f
nua <enuesaa@gmail.com>
2025-01-04 18:51:19 +0900

    refactor(v2): side bar を可変にした

--- src/lib/prototype/CodeViewer.svelte
+++ src/lib/prototype/CodeViewer.svelte
@@ -7,6 +7,8 @@
 	import CodeCopyButton from './CodeCopyButton.svelte'
 	import CodeAreaFileTag from './CodeFileTag.svelte'
 	import CodeTreeItemButton from './CodeTreeItemButton.svelte'
+	import { PaneGroup, Pane, PaneResizer } from 'paneforge'
+	import { Dot } from 'lucide-svelte'
 
 	export let codeFiles: CodeFiles
 	export let firstOpen: string
@@ -22,26 +24,27 @@
 	})
 </script>
 
-{#key $viewing}
-	<section class="rounded-lg bg-editorbg relative">
-		<CodeSep />
+<section class="rounded-lg bg-editorbg relative">
+	<CodeSep />
 
-		<div class="text-base flex">
-			<ul class="flex-none pr-2 pt-2 pb-2 pl-3 border-r-editorsep border-r min-h-[800px] w-2/12 min-w-32 max-w-96 overflow-scroll">
-				{#each codeFiles as treeData}
-					<CodeTreeItemButton data={treeData} />
-				{/each}
-			</ul>
-			<div class="flex-auto relative overflow-scroll">
-				{#if $viewing !== undefined}
-					<Code language={$viewing.language} code={$viewing.code} showLineNumber={true} />
-				{/if}
-			</div>
-		</div>
+	<PaneGroup direction="horizontal" class="text-base flex">
+		<Pane defaultSize={20} class="flex-none pr-2 pt-2 pb-2 pl-3 border-r-editorsep border-r min-h-[800px] min-w-32 max-w-96 overflow-scroll">
+			{#each codeFiles as treeData}
+				<CodeTreeItemButton data={treeData} />
+			{/each}
+		</Pane>
+		<PaneResizer class="w-[1px] bg-gray-600/80 relative">
+			<Dot class="absolute top-1/3 left-[-10px] bg-editorbg text-editortext overflow-visible z-10 w-5" />
+		</PaneResizer>
+		<Pane class="flex-auto relative overflow-scroll">
+			{#if $viewing !== undefined}
+				<Code language={$viewing.language} code={$viewing.code} showLineNumber={true} />
+			{/if}
+		</Pane>
+	</PaneGroup>
 
-		{#if $viewing !== undefined}
-			<CodeAreaFileTag filename={$viewing.title} />
-			<CodeCopyButton text={$viewing.code} />
-		{/if}
-	</section>
-{/key}
+	{#if $viewing !== undefined}
+		<CodeAreaFileTag filename={$viewing.title} />
+		<CodeCopyButton text={$viewing.code} />
+	{/if}
+</section>
