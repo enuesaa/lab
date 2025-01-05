24efd31d346b490e506342275ec0b5000bcbce58
nua <enuesaa@gmail.com>
2025-01-05 18:51:12 +0900

    chore: update content

--- src/routes/prototype/[name]/UnitLinks.svelte
+++ src/routes/prototype/[name]/UnitLinks.svelte
@@ -2,7 +2,7 @@
 	export let links: { title: string; url: string }[]
 </script>
 
-<div class="pl-10 mb-3">
+<div class="pl-10 mt-2 mb-3">
 	{#each links as link}
 		<a href={link.url} class="underline list-item" target="_blank">{link.title}</a>
 	{/each}
