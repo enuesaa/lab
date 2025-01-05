f3eaf7fbb44cfb85913f294f3efe684eb63f23e4
nua <enuesaa@gmail.com>
2025-01-05 18:00:05 +0900

    feat: show publised day on top page.

--- src/routes/ProjectLink.svelte
+++ src/routes/ProjectLink.svelte
@@ -1,27 +1,34 @@
 <script lang="ts">
-	export let href: string
-	export let title: string
+	import type { ProjectV2 } from '$lib/prototype/types'
+
+	export let project: ProjectV2
 </script>
 
 <div>
-	<a {href}>{title}</a>
+	<a href={`/prototype/${project.name}`}>{project.title}</a>
+	<span>{project.published}</span>	
 </div>
 
 <style lang="postcss">
 	div {
-		vertical-align: middle;
-		width: 47%;
-		@apply inline-block h-48 my-3 mx-1 sm:mx-2;
+		@apply inline-block h-48 my-3 mx-1 sm:mx-2 align-middle w-[47%];
+		@apply relative;
 	}
 
 	a {
 		border: solid 1px rgba(0, 0, 0, 0.2);
 		box-shadow: 0 1px 1px rgba(255, 255, 255, 0.2);
-		overflow: hidden;
 		white-space: pre-wrap;
 		@apply flex justify-center items-center;
 		@apply w-full h-full font-zenmaru font-semibold rounded-lg;
 		@apply p-3 sm:p-5 text-lg sm:text-xl;
 		@apply hover:bg-grayblacker;
+		@apply overflow-hidden;
+	}
+
+	span {
+		letter-spacing: 1px;
+		@apply absolute bottom-0 right-0 px-2;
+		@apply text-right text-base font-semibold font-zenmaru text-gray-800/80;
 	}
 </style>
