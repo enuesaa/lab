32091ea06272fb199a1700f3f5a2104ff83d408c
nua <enuesaa@gmail.com>
2025-01-11 17:34:42 +0900

    feat(img): zoom in and move

--- src/routes/prototype/[name]/UnitImageOverlay.svelte
+++ src/routes/prototype/[name]/UnitImageOverlay.svelte
@@ -1,27 +1,35 @@
 <script lang="ts">
 	import { createDialog, melt } from '@melt-ui/svelte'
-	import { MoveDiagonal, X } from 'lucide-svelte'
-	import { fade } from 'svelte/transition'
+	import { MoveDiagonal, X as CloseIcon } from 'lucide-svelte'
+	import type { MouseEventHandler } from 'svelte/elements'
 
 	export let projectName: string
 	export let image: string
 
-	// let zoomLevel: '100'|'150' = '100'
-	// const zoomLevelStyleMap = {
-	// 	'100': 'scale-90',
-	// 	'150': 'scale-150',
-	// }
-	// function handleZoom() {
-	// 	if (zoomLevel === '100') {
-	// 		zoomLevel = '150'
-	// 	} else {
-	// 		zoomLevel = '100'
-	// 	}
-	// }
 	const {
 		elements: { trigger, overlay, portalled, close, content },
 		states: { open },
-	} = createDialog({ })
+	} = createDialog()
+
+	let imgRef: HTMLImageElement
+	let scale = 1
+	let originX = 50
+	let originY = 50
+	const handleZoom = () => {
+		scale = (scale === 1) ? 1.3 : 1
+	}
+	const handleMouseMove: MouseEventHandler<HTMLDivElement> = (e) => {
+		const rect = e.currentTarget.getBoundingClientRect()
+		originX = ((e.clientX - rect.left) / rect.width) * 100
+		originY = ((e.clientY - rect.top) / rect.height) * 100
+	}
+	$: {
+		if (imgRef !== undefined) {
+			imgRef.style.transformOrigin = `${originX}% ${originY}%`
+			imgRef.style.transform = `scale(${scale})`
+			imgRef.style.cursor = scale === 1 ? 'zoom-in' : 'zoom-out'
+		}
+	}
 </script>
 
 <button use:melt={$trigger} class="absolute top-1 right-1 text-gray-700">
@@ -30,24 +38,38 @@
 
 {#if $open}
 	<div use:melt={$portalled}>
-		<div use:melt={$overlay}
-			class="fixed inset-0 bg-gray-900/90"
-			transition:fade={{ duration: 70 }}
-		></div>
+		<div use:melt={$overlay} class="fixed inset-0 bg-gray-900/90 fade"></div>
+		
+		<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 		<img use:melt={$content}
 			src={`/prototype/${projectName}/${image}`}
 			alt={image}
-			transition:fade={{ duration: 70 }}
+			bind:this={imgRef}
+			on:mousemove={handleMouseMove}
+			on:click={handleZoom}
 		/>
+
 		<button use:melt={$close} class="fixed text-gray top-1 right-1">
-			<X class="w-8 h-8" />
+			<CloseIcon class="w-8 h-8" />
 		</button>
 	</div>
 {/if}
 
 <style lang="postcss">
+	.fade {
+		animation: fade 1s ease forwards;
+	}
+	@keyframes fade {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+
 	img {
-		@apply block w-full max-w-[1500px];
-		@apply fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20;
+		@apply fixed z-20 block w-10/12 inset-0 m-auto max-w-[1500px];
+		@apply fade;
 	}
 </style>
