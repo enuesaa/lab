c1f4675aa8d6e72c40ab05ae4ceb0e8b4ecc8f0f
nua <enuesaa@gmail.com>
2025-06-08 18:48:35 +0900

    feat: 余白を調整

--- src/routes/prototype/[name]/UnitTitle.svelte
+++ src/routes/prototype/[name]/UnitTitle.svelte
@@ -13,7 +13,7 @@
 	}
 </script>
 
-<div class="mt-5 mb-[6px] mx-auto px-3 font-semibold text-2xl font-zenmaru">
+<div class="mt-6 mb-[6px] mx-auto px-3 font-semibold text-2xl font-zenmaru">
 	{#if symbol === undefined}
 		<Diamond class="inline-block align-bottom w-[14px] fill-gray-1000 stroke-gray-1000 pb-[4px] mr-[6px]" />	
 	{:else}
