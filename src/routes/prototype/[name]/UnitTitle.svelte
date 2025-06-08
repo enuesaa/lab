3aa9807235f892d66d851a431215114b8051c576
nua <enuesaa@gmail.com>
2025-06-08 18:35:11 +0900

    feat: title に symbol を入れられるようにした

--- src/routes/prototype/[name]/UnitTitle.svelte
+++ src/routes/prototype/[name]/UnitTitle.svelte
@@ -2,8 +2,22 @@
 	import { Diamond } from 'lucide-svelte'
 
 	export let title: string
+
+	let symbol: string|undefined = undefined
+	let display = title
+
+	const match = title.match(/^symbol\(([^)]+)\)\s+(.+)$/)
+	if (match !== null && match.length > 0) {
+		symbol = match[1]
+		display = match[2]
+	}
 </script>
 
-<div class="mt-4 mb-[6px] mx-auto px-3 font-semibold text-2xl font-zenmaru">
-	<Diamond class="inline-block align-bottom w-[14px] fill-gray-1000 stroke-gray-1000 pb-[4px] mr-[6px]" />{title}
+<div class="mt-5 mb-[6px] mx-auto px-3 font-semibold text-2xl font-zenmaru">
+	{#if symbol === undefined}
+		<Diamond class="inline-block align-bottom w-[14px] fill-gray-1000 stroke-gray-1000 pb-[4px] mr-[6px]" />	
+	{:else}
+		<span class="font-bold text-[1.6rem]">{symbol}</span>
+	{/if}
+	{display}
 </div>
