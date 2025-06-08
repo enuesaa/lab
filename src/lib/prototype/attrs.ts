4c3be3b75246a9737c9eaefc618d0e79e80d1a0b
nua <enuesaa@gmail.com>
2025-06-08 19:43:37 +0900

    feat: parse attrs

---
+++ src/lib/prototype/attrs.ts
@@ -0,0 +1,25 @@
+type Parsed = {
+  content: string
+  attrs: {
+    [key: string]: string
+  }
+}
+
+export const parseAttrs = (text: string): Parsed => {
+  const res = {
+    content: '',
+    attrs: {},
+  } as Parsed
+  if (text === '') {
+    return res
+  }
+
+  const matched = text.matchAll(/(\w+)=\(([^)]+)\)/g)
+  for (const [_, key, value] of matched) {
+    res.attrs[key] = value
+    text = text.replace(`${key}=(${value})`, '')
+  }
+  res.content =  text.trimStart()
+
+  return res
+}
