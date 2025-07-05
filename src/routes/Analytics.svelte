69c4b336d9c209cd91e38dfe704b2adae4febde9
nua <enuesaa@gmail.com>
2025-07-05 19:12:21 +0900

    chore: ga を dev でのみ読み込むようにした

--- src/routes/Analytics.svelte
+++ src/routes/Analytics.svelte
@@ -1,13 +1,17 @@
+<script lang="ts">
+  const isDev = import.meta.env.DEV
+</script>
+
 <svelte:head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id="></script>
-  <script>
-    window.dataLayer = window.dataLayer || []
-
-    function gtag() {
-      dataLayer.push(arguments)
-    }
-
-    gtag('js', new Date())
-    gtag('config', '')
-  </script>
+  {#if !isDev}
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-M85LPSR46E"></script>
+    <script>
+      window.dataLayer = window.dataLayer || []
+      function gtag() {
+        dataLayer.push(arguments)
+      }
+      gtag('js', new Date())
+      gtag('config', 'G-M85LPSR46E')
+    </script>
+  {/if}
 </svelte:head>
