22e641c3075f4cb5494d7427ebeb9d58eb4aad49
nua <enuesaa@gmail.com>
2025-08-02 20:12:43 +0900

    feat: write coder server template management

---
+++ data/coder-server-templates-managed-by-terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    coderd = {
+      source = "coder/coderd"
+    }
+  }
+}
+
+variable "token" {
+  description = "Coder authentication token"
+  type        = string
+  sensitive   = true
+}
+
+provider "coderd" {
+  url   = "http://127.0.0.1:3000"
+  token = var.token
+}
+
+resource "coderd_template" "minimum" {
+  name = "minimum"
+
+  description = "minimum template"
+
+  versions = [
+    {
+      name      = "v1"
+      directory = "templates/minimum"
+      active    = true
+    },
+  ]
+}
