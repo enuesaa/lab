8cd5f4ffc74469f1c72541be81151e722004eeef
nua <enuesaa@gmail.com>
2025-10-13 00:15:50 +0900

    feat: update zig code

--- data/zig-0.15-subshell/src/main.zig
+++ data/zig-0.15-subshell/src/main.zig
@@ -3,14 +3,19 @@ const std = @import("std");
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
+    // zsh を立ち上げる
     const argv = &[_][]const u8{"zsh"};
     var child = std.process.Child.init(argv, allocator);
+
+    // カレントディレクトリで
     child.cwd_dir = std.fs.cwd();
 
+    // 環境変数をセット
     var envmap = try std.process.getEnvMap(allocator);
     try envmap.put("AAA", "bbb");
     child.env_map = &envmap;
 
-    const exit_code = try child.spawnAndWait();
-    std.debug.print("Exit code: {}\n", .{exit_code});
+    // 立ち上げ
+    const term = try child.spawnAndWait();
+    std.debug.print("exit code: {d}\n", .{term.Exited});
 }
