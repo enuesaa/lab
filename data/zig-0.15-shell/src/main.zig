9456786ac6a39d5e7c16d581a5c2f79d8bf9af47
nua <enuesaa@gmail.com>
2025-10-19 19:17:19 +0900

    fix(zig): memory leak していた

--- data/zig-0.15-shell/src/main.zig
+++ data/zig-0.15-shell/src/main.zig
@@ -1,7 +1,9 @@
 const std = @import("std");
 
 pub fn main() !void {
-    const allocator = std.heap.page_allocator;
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
 
     // zsh を立ち上げる
     const argv = &[_][]const u8{"zsh"};
@@ -13,6 +15,7 @@ pub fn main() !void {
     // 環境変数をセット
     var envmap = try std.process.getEnvMap(allocator);
     try envmap.put("AAA", "bbb");
+    defer envmap.deinit();
     child.env_map = &envmap;
 
     // 立ち上げ
