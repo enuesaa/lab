d06b288331aeacef1152c5ef5adc20fccad57b96
nua <enuesaa@gmail.com>
2026-01-10 18:54:32 +0900

    chore: starrel のコードを持ってきた

---
+++ data/azure-static-web-apps-with-api/api/db/repos.py
@@ -0,0 +1,48 @@
+from azure.cosmos.errors import CosmosResourceNotFoundError
+from typing import List
+from db.conn import connect
+from db.models import Bookmark
+
+def list_bookmarks() -> List[Bookmark]:
+    db = connect()
+    items = db.query_items(
+        query="SELECT * FROM c WHERE c.type = @type",
+        parameters=[
+            {"name": "@type", "value": "favorite"},
+        ],
+        enable_cross_partition_query=False,
+    )
+    return [Bookmark.model_validate(d) for d in items]
+
+def search_bookmarks(keyword: str) -> List[Bookmark]:
+    db = connect()
+    items = db.query_items(
+        query="SELECT * FROM c WHERE c.type = @type AND (CONTAINS(c.url, @kw) OR CONTAINS(c.title, @kw) OR CONTAINS(c.description, @kw))",
+        parameters=[
+            {"name": "@type", "value": "favorite"},
+            {"name": "@kw", "value": keyword},
+        ],
+        enable_cross_partition_query=False
+    )
+    return [Bookmark.model_validate(d) for d in items]
+
+def get_bookmark(id: str) -> Bookmark|None:
+    db = connect()
+    try:
+        data = db.read_item(
+            item=id,
+            partition_key="favorite",
+        )
+    except CosmosResourceNotFoundError:
+        return None
+    return Bookmark.model_validate(data)
+
+def upsert_bookmark(bookmark: Bookmark) -> Bookmark:
+    bookmark.type = "favorite"
+    db = connect()
+    data = db.upsert_item(bookmark.model_dump(by_alias=True))
+    return Bookmark.model_validate(data)
+
+def delete_bookmark(id: str) -> None:
+    db = connect()
+    db.delete_item(item=id, partition_key="favorite")
