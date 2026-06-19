4f0ee1dedf8c495e4633429693f130ff42346a38
nua <enuesaa@gmail.com>
2026-06-19 23:31:35 +0900

    chore: update

--- data/m5stack-cores3-aws-iot-core-register/src/vars.hpp
+++ data/m5stack-cores3-aws-iot-core-register/src/vars.hpp
@@ -1,21 +1,22 @@
+// wifi
 static const char *WIFI_SSID = "";
 static const char *WIFI_PASSWORD = "";
 
-// curl https://www.amazontrust.com/repository/AmazonRootCA1.pem
+// https://www.amazontrust.com/repository/AmazonRootCA1.pem より。
 static const char *AWSIOT_ROOT_CA = R"(-----BEGIN CERTIFICATE-----
 -----END CERTIFICATE-----
 )";
 
-// file name is xx.cert.pem
+// 接続キットより。ファイル名は <name>.cert.pem
 static const char *AWSIOT_CERTIFICATE = R"(-----BEGIN CERTIFICATE-----
 -----END CERTIFICATE-----
 )";
 
-// file name is xx.private.key
+// 接続キットより。ファイル名は <name>.private.key
 static const char *AWSIOT_PRIVATE_KEY = R"(-----BEGIN RSA PRIVATE KEY-----
 -----END RSA PRIVATE KEY-----
 )";
 
-// look up aws iot core console
+// IoT Core のエンドポイント。AWSコンソールに記載
 static const char *AWSIOT_ENDPOINT = "xx.amazonaws.com";
 static const char *AWSIOT_THING_ID = "";
