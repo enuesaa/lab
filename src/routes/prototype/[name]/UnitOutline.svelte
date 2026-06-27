c1a17e0bc83db3c5b3790a494156fbfb7cf1e4f1
nua <enuesaa@gmail.com>
2026-06-27 19:21:55 +0900

    chore: add outline to another page

--- src/routes/prototype/[name]/UnitOutline.svelte
+++ src/routes/prototype/[name]/UnitOutline.svelte
@@ -4,7 +4,7 @@
 	export let outline: Outline
 </script>
 
-<nav class="m-6 w-fit min-w-[260px] border rounded-xl p-5 font-zenmaru text-blackgray/85">
+<nav class="my-9 w-fit min-w-[260px] border rounded-xl p-5 font-zenmaru text-blackgray/85">
 	<div class="text-2xl mb-3 font-medium">{outline?.title ?? '目次'}</div>
 	<ol class="flex flex-col list-none m-0 p-0">
 		{#each outline.items ?? [] as item}
