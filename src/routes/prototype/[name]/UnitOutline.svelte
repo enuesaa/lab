819cea6c4b3df9ce04e2f7d03483f1912c239b3f
nua <enuesaa@gmail.com>
2026-06-27 19:25:53 +0900

    chore: change style

--- src/routes/prototype/[name]/UnitOutline.svelte
+++ src/routes/prototype/[name]/UnitOutline.svelte
@@ -4,12 +4,12 @@
 	export let outline: Outline
 </script>
 
-<nav class="my-9 w-fit min-w-[260px] border rounded-xl p-5 font-zenmaru text-blackgray/85">
+<nav class="my-9 w-fit min-w-[260px] border border-blackgray/80 rounded-xl p-5 font-zenmaru text-blackgray/85 bg-graywhite">
 	<div class="text-2xl mb-3 font-medium">{outline?.title ?? '目次'}</div>
 	<ol class="flex flex-col list-none m-0 p-0">
 		{#each outline.items ?? [] as item}
 			<li>
-				<a href="#{item.as}" class="flex items-center gap-1 p-2 rounded-xl text-lg no-underline font-medium hover:bg-graywhite/80">
+				<a href="#{item.as}" class="flex items-center gap-1 p-2 rounded-xl text-lg no-underline font-medium hover:bg-gray">
 					{item.title}
 				</a>
 			</li>
