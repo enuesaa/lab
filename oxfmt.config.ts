d7043d7af5f3085533ffcf6b01c373d675d0e1a5
nua <enuesaa@gmail.com>
2026-06-27 16:17:10 +0900

    feat: use oxfmt

---
+++ oxfmt.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'oxfmt'
+
+export default defineConfig({
+	printWidth: 120,
+	tabWidth: 2,
+	semi: false,
+	singleQuote: true,
+	endOfLine: 'lf',
+	trailingComma: 'es5',
+	useTabs: true,
+})
