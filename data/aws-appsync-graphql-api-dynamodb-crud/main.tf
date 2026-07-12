ba040c55c062865dce2003595649c160b5cb9dd1
nua <enuesaa@gmail.com>
2026-07-12 19:47:10 +0900

    chore: add main.tf

--- data/aws-appsync-graphql-api-dynamodb-crud/main.tf
+++ data/aws-appsync-graphql-api-dynamodb-crud/main.tf
@@ -1,10 +1,26 @@
 terraform {
+  required_version = "1.15.8"
+
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+
+  # backend "s3" {
+  #   bucket = ""
+  #   key    = ""
+  #   region = "ap-northeast-1"
+  # }
+}
+
+provider "aws" {
+  region = "ap-northeast-1"
+
+  default_tags {
+    tags = {
+      terraform = "aws-appsync-graphql-api-dynamodb-crud"
     }
   }
 }
-
-data "aws_caller_identity" "main" {}
-data "aws_region" "main" {}
