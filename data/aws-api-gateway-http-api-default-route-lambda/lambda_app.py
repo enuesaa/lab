6d776699b576155c3fcf793ac669ca95f82405fd
nua <enuesaa@gmail.com>
2026-07-25 19:54:32 +0900

    chore: update

--- data/aws-api-gateway-http-api-default-route-lambda/lambda_app.py
+++ data/aws-api-gateway-http-api-default-route-lambda/lambda_app.py
@@ -1,8 +1,8 @@
 def lambda_handler(event, context):
     print('routeKey:', event['routeKey'])
     print('rawPath:', event['rawPath'])
-    print('queryStringParameters:', event['queryStringParameters'])
-    print('body:', event['body'])
+    print('queryStringParameters:', event.get('queryStringParameters', ''))
+    print('body:', event.get('body', ''))
     print('requestContext.http.method:', event['requestContext']['http']['method'])
     print('requestContext.http.path:', event['requestContext']['http']['path'])
 
