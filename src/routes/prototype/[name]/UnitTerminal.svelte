52835aebcc1335589759e34232c12e2211763347
nua <enuesaa@gmail.com>
2026-07-25 20:17:38 +0900

    fix: 横スクロールできなかった

--- src/routes/prototype/[name]/UnitTerminal.svelte
+++ src/routes/prototype/[name]/UnitTerminal.svelte
@@ -4,6 +4,8 @@
 	export let content: string
 </script>
 
-<div class="p-4 bg-editorbg rounded-lg w-fit min-w-[50%]">
-	<Code language="console" code={content} />
+<div class="overflow-x-auto">
+	<div class="p-4 bg-editorbg rounded-lg w-fit min-w-[50%]">
+		<Code language="console" code={content} />
+	</div>
 </div>
