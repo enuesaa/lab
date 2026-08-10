2a5a0f75c5a1bc7f097f07ffad6289f1f70947ea
nua <enuesaa@gmail.com>
2026-08-10 21:34:50 +0900

    chore: バリデーション

--- data/php-symfony-memo-app/src/Form/MemoType.php
+++ data/php-symfony-memo-app/src/Form/MemoType.php
@@ -5,8 +5,11 @@ namespace App\Form;
 use App\Entity\Memo;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 // see https://symfony.com/doc/current/forms.html
 class MemoType extends AbstractType
@@ -14,8 +17,18 @@ class MemoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title', TextType::class)
-            ->add('description', TextType::class);
+            ->add('title', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(min: 3, minMessage: '3文字以上で入力してください'),
+                ],
+            ])
+            ->add('description', TextareaType::class, [
+                'attr' => ['rows' => 8],
+                'constraints' => [
+                    new Length(max: 100, maxMessage: '100文字以内で入力してください'),
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
