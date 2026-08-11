2b1e287e4a4631ed2fe3adb2f94b9c351e3cbb79
nua <enuesaa@gmail.com>
2026-08-11 16:51:16 +0900

    chore: update

--- data/php-symfony-memo-app/src/Form/MemoType.php
+++ data/php-symfony-memo-app/src/Form/MemoType.php
@@ -11,7 +11,6 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
 
-// see https://symfony.com/doc/current/forms.html
 class MemoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
