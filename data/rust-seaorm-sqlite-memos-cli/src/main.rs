441895bb334a379f10b0b8c49309c6d10c232cfb
nua <enuesaa@gmail.com>
2026-08-29 18:09:29 +0900

    chore: add gif

--- data/rust-seaorm-sqlite-memos-cli/src/main.rs
+++ data/rust-seaorm-sqlite-memos-cli/src/main.rs
@@ -11,8 +11,11 @@ async fn main() -> Result<()> {
     let db = db::connect(args.dbpath).await?;
 
     match args.command.unwrap_or(cli::Command::List) {
+        // メモ一覧
         cli::Command::List => cli::handler::list(&db).await?,
+        // メモ作成
         cli::Command::New => cli::handler::new(&db).await?,
+        // メモ削除
         cli::Command::Delete => cli::handler::delete(&db).await?,
     }
     Ok(())
