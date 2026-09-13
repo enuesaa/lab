8f58831d9d343156936571326e96a5e9de2930bc
nua <enuesaa@gmail.com>
2026-09-14 00:18:15 +0900

    refactor: DynamoDbLocalSetupCommand

---
+++ data/php-symfony-memos-app-lambda-dynamodb/src/Command/DynamoDbLocalSetupCommand.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Command;
+
+use Aws\DynamoDb\DynamoDbClient;
+use Aws\DynamoDb\Exception\DynamoDbException;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(name: 'app:dynamodb-local:setup', description: 'Creates the memos table in DynamoDB Local, if it does not already exist.')]
+class DynamoDbLocalSetupCommand extends Command
+{
+    public function __construct(
+        private readonly DynamoDbClient $client,
+        private readonly string $tableName,
+        private readonly ?string $endpoint,
+    ) {
+        parent::__construct();
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $io = new SymfonyStyle($input, $output);
+
+        if (!$this->endpoint) {
+            $io->error('DYNAMODB_ENDPOINT is not set; this command only targets DynamoDB Local.');
+
+            return Command::FAILURE;
+        }
+
+        try {
+            $this->client->describeTable(['TableName' => $this->tableName]);
+            $io->success("Table \"{$this->tableName}\" already exists, nothing to do.");
+
+            return Command::SUCCESS;
+        } catch (DynamoDbException $e) {
+            if ('ResourceNotFoundException' !== $e->getAwsErrorCode()) {
+                throw $e;
+            }
+        }
+
+        $this->client->createTable([
+            'TableName' => $this->tableName,
+            'AttributeDefinitions' => [
+                ['AttributeName' => 'id', 'AttributeType' => 'S'],
+            ],
+            'KeySchema' => [
+                ['AttributeName' => 'id', 'KeyType' => 'HASH'],
+            ],
+            'BillingMode' => 'PAY_PER_REQUEST',
+        ]);
+
+        $this->client->waitUntil('TableExists', ['TableName' => $this->tableName]);
+
+        $io->success("Table \"{$this->tableName}\" created.");
+
+        return Command::SUCCESS;
+    }
+}
